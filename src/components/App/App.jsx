4f9fbce3439a448225921b2510692adcb387528d
PinalBhimani006 <pinalpbhimani123@gmail.com>
2021-01-22 14:36:32 +0530

    Code revised

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Router } from "react-router-dom";
 import { Asteroid } from "./../Asteroid/Asteroid";
 import { Navbar } from "./../Navbar/Navabar";
+import { AsteroidDetail } from "./../AsteroidDetail/AsteroidDetail";
 import history from "./../../routes/history";
 import React from "react";
 import * as route from "./../../routes/route";
@@ -13,6 +14,9 @@ function App() {
       <Router history={history}>
         <Route path={route.home} exact>
           <Asteroid />
+        </Route>
+        <Route path={route.asteroidDetail} exact>
+          <AsteroidDetail />
         </Route>
       </Router>
     </div>
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,1 +1,2 @@
 export const home = "/"
+export const asteroidDetail = "/asteroid/detail"
